cc63310658c0323cc69a9412bd727303e4c4e902
shachar <info@web-college.co.il>
2025-09-14 19:29:22 +0300

    add seeder for products

--- seeders/20250907191611-add-new-products.js
+++ seeders/20250907191611-add-new-products.js
@@ -1,26 +1,23 @@
 'use strict';
-
+const productsData = require('../data/products.json');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-    */
 
-    await queryInterface.bulkInsert('products', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
+    const today = new Date();
+    const productsToAdd = productsData.map(p => {
+      return {
+        ...p,
+        sku: p.id,
+        created_at: today,
+        updated_at: today
+      }
+
+    });
+    await queryInterface.bulkInsert('products', productsToAdd, {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+      await queryInterface.bulkDelete('products', null, {});
   }
 };
